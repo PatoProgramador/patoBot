6aa8bd3022b3287829e21327ccbefbe025294f5c
PatoProgramador <spartan1171947@hotmail.com>
2023-04-20 23:24:37 -0500

    commands and events handler

---
+++ handlers/events.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+const allEvents = [];
+
+module.exports = async (client) => {
+    try {
+        try {
+            console.log('Cargando los eventos...')
+        } catch (error) {}
+        let cantidad = 0;
+        const cargar_dir = (dir) => {
+            const archivos_eventos = fs.readFileSync(`./eventos/${dir}`).filter((file) => file.endsWith('.js'))
+            for(const archivo of archivos_eventos) {
+                try {
+                   const evento = require(`../eventos/${dir}/${archivo}`) 
+                   const nombre_evento = archivo.split(".")[0];
+                   allEvents.push(nombre_evento);
+                   client.on(nombre_evento, evento.bind(null, client));
+                   cantidad++
+                } catch (error) {
+                    console.log(error)
+                }
+            }
+        }
+        await ["client", "guild"].forEach(e => cargar_dir(e))
+        console.log(`${cantidad} Eventos cargados`)
+        try {
+            console.log("Iniciando sesión del Bot...")
+        } catch (error) {
+            console.log(error)
+        }
+    } catch (error) {
+        console.log(error)
+    }
+};
--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-const { Client } = require('discord.js');
+const Discord  = require('discord.js');
 require('dotenv').config();
 
-const bot = new Client({ intents: [131071]});
+const bot = new Discord.Client({ intents: [131071]});
 
-bot.login(process.env.DS_BOT_TOKEN).then(() => {
-    console.log(`El bot ${bot.user.username} se ha iniciado :)`)
-    bot.user.setActivity('Mimiendo');
-}).catch((err) => console.log(err));
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+
+function requestHandlers() {
+    ["command", "events"].forEach(handler => {
+        try {
+            require(`./handlers/${handler}`)(client, Discord)
+        } catch (error) {
+            console.warn(error)
+        }
+    })
+}
+requestHandlers();
+
+bot
+    .login(process.env.DS_BOT_TOKEN)
+    .then(() => {
+        console.log(`El ${bot.user.username} se ha iniciado :)`)
+        bot.user.setActivity('existir');
+    })
+    .catch((err) => console.log(err));
