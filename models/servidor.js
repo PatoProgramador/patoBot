abd37fd7f99349f8990454edf850e933027f3fd1
PatoProgramador <spartan1171947@hotmail.com>
2023-04-21 16:18:38 -0500

    server model

--- models/servidor.js
+++ models/servidor.js
@@ -1,1 +1,10 @@
-const mongoose = require
+const mongoose = require('mongoose');
+
+const serverSchema = new mongoose.Schema({
+    guildID: String,
+    prefijo: String
+});
+
+const model = mongoose.model('ConfigServer', serverSchema);
+
+module.exports = model;
