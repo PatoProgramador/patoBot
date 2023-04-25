f8f2302065a0c8f4d625950424c6410d0047c1b8
PatoProgramador <spartan1171947@hotmail.com>
2023-04-24 22:03:16 -0500

    Fix aliases bug

--- comandos/info/ping.js
+++ comandos/info/ping.js
@@ -1,4 +1,4 @@
-module.exports = {
+module.exports  = {
     name: "ping",
     aliases: ["latencia", "ms"],
     desc: "Sirve para ver la latencia del bot",
--- eventos/guild/messageCreate.js
+++ eventos/guild/messageCreate.js
@@ -11,32 +11,26 @@ module.exports = async (client, message) => {
     });
     if (!message.content.startsWith(data.prefijo) || message.author.bot) return;
 
-    // Dividimos el mensaje en un array y tomamos el primer elemento como el comando.
+    // Buscar el comando en la colección de comandos
     const args = message.content.slice(data.prefijo.length).trim().split(/ +/);
-    const commandName = args.shift().toLowerCase();
-  
-    // Buscamos el comando en la colección de comandos del bot.
-    const command = client.commands.get(commandName);
-  
-    // Si el comando no existe, enviamos un mensaje indicando que no se reconoce el comando.
-    if (!command) {
-      return message.reply(`El comando ${commandName} no existe. Intenta con ${data.prefijo}help para ver la lista de comandos.`);
+    // const args = message.content.slice(data.prefijo.length).trim().split(" ");
+    const commandName = args.shift()?.toLowerCase();
+    const command = client.commands.find(cmd => cmd.name == commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName))
+
+    let cmdFunction;
+
+    if (command && typeof command == "object") {
+      cmdFunction = command.run;
+    } else {
+      // Si el comando no existe, mostrar un mensaje de error
+      return message.reply(`El comando "${commandName}" no existe. Por favor, use \`${data.prefijo}help\` para ver una lista de comandos disponibles.`);
     }
-  
+    
+    // Ejecutar el comando
     try {
-      // Ejecutamos el comando y manejamos cualquier posible error.
-      await command.run(client, message, args, data.prefijo);
+      await cmdFunction(client, message, args, data.prefijo);
     } catch (error) {
       console.error(error);
       message.reply('¡Ups! Ha ocurrido un error al ejecutar este comando.');
     }
-    // if (!message.content.startsWith(data.prefijo)) return;
-    // const args = message.content.slice(data.prefijo.length).trim().split(" ");
-    // const cmd = args.shift()?.toLowerCase();
-    // const command = client.commands.get(cmd) || client.commands.find(c => c.aliases && c.alises.includes(cmd))
-    // if (command) {
-    //     command.run(client, message, args, data.prefijo)
-    // } else {
-    //     return message.reply("No he encontrado el comando especificado")
-    // }
 };
